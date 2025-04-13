9374e63c8a3ed3e60281510687d351cfd93ca94e
AMIR\amirm <amirmahdifalahiword@gmail.com>
2025-04-13 10:15:34 -0700

    five change

--- src/app/pages/signin/signin.page.ts
+++ src/app/pages/signin/signin.page.ts
@@ -1,33 +1,91 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormGroup, Validators, ReactiveFormsModule, FormsModule, FormBuilder } from '@angular/forms';
 import { Router } from '@angular/router';
+import { LoadingController, ToastController } from '@ionic/angular';
+import { IonContent, IonRow, IonCol, IonNote, IonText, IonGrid } from '@ionic/angular/standalone';
+
 
 @Component({
   selector: 'app-signin',
   templateUrl: './signin.page.html',
   styleUrls: ['./signin.page.scss'],
   standalone: true,
-  imports: [ CommonModule, ReactiveFormsModule, FormsModule ]
+  imports: [ CommonModule, IonContent, IonGrid, IonRow, IonCol, IonNote, IonText]
 })
 
 export class SigninPage implements OnInit {
-  loginForm!: FormGroup;
-  router = inject(Router);
 
-  constructor(private fb: FormBuilder) {}
+otp:string="";
+router = inject(Router);
 
-  ngOnInit() {
-    this.loginForm = this.fb.group({
-      email: ['', [Validators.required, Validators.email]],
-      password: ['', [Validators.required, Validators.minLength(8)]],
-    });
+constructor(public loadingctrl:LoadingController, public toastctrl:ToastController) { }
+
+ ngOnInit() {
+    this.setipfocuse();
+ }
+
+  setipfocuse() {
+    for(let i = 1; i <=6; i++) {
+      if((this.otp.length + 1) == i) {
+        document.getElementById("ip" + i)?.style.background =="var(--ion-color-dark)"
+      }
+      else {
+        document.getElementById("ip" + i)?.style.background =="var(--ion-color-light)"
+      }
+    }
   }
 
-  onSubmit() {
-      if (this.loginForm.valid) {
-        console.log('Login data', this.loginForm.value);
-        this.router.navigateByUrl("/home/profile");
+  clear() {
+    this.otp = "";
+    this.setipfocuse();
+  }
+
+  back() {
+    this.otp = this.otp.slice(0, -1);
+    this.setipfocuse();
+  }
+
+  set(number:any) {
+    this.otp += number;
+    this.setipfocuse();
+
+    if(this.otp.length == 6) {
+      this.presentloading();
+      this.checkotp();
+    }
+  }
+
+  async presentloading() {
+    const loading = await this.loadingctrl.create({
+      message: 'verifing otp',
+      spinner: "circular" 
+    });
+    await loading.present();
+  }
+
+  async presenttoast(message:any, color:any) {
+    const toast = await this.toastctrl.create({
+      message:message,
+      color:color,
+      duration:1000,
+      position:"middle"
+    });
+    toast.present();
+  }
+
+  checkotp() {
+    setTimeout(() => {
+      this.loadingctrl.dismiss();
+
+      if(this.otp == "123456") {
+        this.presenttoast("otp vrified" , "success");
+        this.router.navigateByUrl("home/profile");
+        this.clear();
       }
+      else {
+        this.presenttoast("invalid otp" , "danger");
+        this.clear();
+      }
+    },2000)
 }
 }
